b96e7d292b6aa72b1ca88fa127c3d2a29dd889c2
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-20 08:35:27 +0000

    okay

--- db/account.py
+++ db/account.py
@@ -8,27 +8,27 @@ class Account:
 
     async def prepare_table(self):
         "Creates the accounts table if it doesn't exist."
-        await self.cursor.execute("""CREATE TABLE IF NOT EXISTS accounts (
+        await self.cursor.execute("""CREATE TABLE IF NOT EXISTS Accounts (
             id INT PRIMARY KEY, username TEXT, token VARCHAR(255)
         );""")
 
     async def create_account(self, userid: int, username: str, token: str):
         "Creates an account."
         await self.cursor.execute(
-            "INSERT INTO accounts (id, username, token) VALUES (%s, %s, %s);",
+            "INSERT INTO Accounts (id, username, token) VALUES (%s, %s, %s);",
             (userid, username, token)
         )
     
     async def get_user(self, userid: int):
         "Gets an account."
         await self.cursor.execute(
-            "SELECT * FROM accounts WHERE id = %s;", (userid,)
+            "SELECT * FROM Accounts WHERE id = %s;", (userid,)
         )
         return await self.cursor.fetchone()
     
     async def get_account(self, token: str):
         "Gets an account."
         await self.cursor.execute(
-            "SELECT * FROM accounts WHERE token = %s;", (token,)
+            "SELECT * FROM Accounts WHERE token = %s;", (token,)
         )
         return await self.cursor.fetchone()
